88b15214426e24684425680d8b00cc8c59a30c9a
Marianna Cruz <mariannactx@gmail.com>
2019-02-13 19:36:09 -0300

    Adicionar tabela de acervo

--- database/sql/001.sql
+++ database/sql/001.sql
@@ -7,15 +7,14 @@ CREATE TABLE `videos_personalizados` (
   PRIMARY KEY (`id`),
   UNIQUE INDEX `arquivo` (`arquivo` ASC));
 
-ALTER TABLE `acervo` 
-CHANGE COLUMN `arquivo` `arquivo` VARCHAR(45) NOT NULL ,
-ADD COLUMN `titulo` VARCHAR(45) NOT NULL AFTER `id`,
-ADD COLUMN `descricao` VARCHAR(45) NOT NULL AFTER `titulo`,
-ADD COLUMN `thumb` VARCHAR(45) NULL AFTER `arquivo`,
-ADD UNIQUE INDEX `arquivo_UNIQUE` (`arquivo` ASC);
-
-ALTER TABLE `acervo` 
-AUTO_INCREMENT = 1 ;
+CREATE TABLE `acervo` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`titulo` VARCHAR(45) NOT NULL,
+`descricao` VARCHAR(45) NOT NULL,
+`arquivo` VARCHAR(45) NOT NULL,
+`thumb` VARCHAR(45) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `arquivo` (`arquivo` ASC));
 
 INSERT INTO `acervo` (`titulo`, `descricao`, `arquivo`, `thumb`) VALUES ('Pioneirismo do TCE', 'O pioneirismo do TCE', 'pioneirismo_tce.mp4', 'pioneirismo_tce.png');
 
